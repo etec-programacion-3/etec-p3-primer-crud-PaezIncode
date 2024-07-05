7e23038d178a92326358eab9ed0dca9ce6c7eeca
PaezIncode <nico.paezdelgadillo@gmail.com>
2024-07-05 19:07:53 -0300

    Documentar

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ const app = express();
 const port = 3000;
 
 config();
-const filename = "database.db"
-console.log(filename)
+const filename = "database.db";
+console.log(filename);
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: filename
 });
 
+/**
+ * Modelo que representa un libro en la base de datos.
+ * 
+ * @class Book
+ * @extends {Model}
+ */
 class Book extends Model { }
+
 Book.init({
     autor: DataTypes.STRING,
     isbn: DataTypes.INTEGER,
@@ -27,21 +34,49 @@ sequelize.sync();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+/**
+ * Ruta para obtener todos los libros.
+ * 
+ * @route GET /books
+ * @param {Request} req - La solicitud HTTP.
+ * @param {Response} res - La respuesta HTTP.
+ */
 app.get('/books', async (req, res) => {
     const books = await Book.findAll();
     res.json(books);
 });
 
+/**
+ * Ruta para obtener un libro por ID.
+ * 
+ * @route GET /books/:id
+ * @param {Request} req - La solicitud HTTP.
+ * @param {Response} res - La respuesta HTTP.
+ */
 app.get('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     res.json(book);
 });
 
+/**
+ * Ruta para crear un nuevo libro.
+ * 
+ * @route POST /books
+ * @param {Request} req - La solicitud HTTP.
+ * @param {Response} res - La respuesta HTTP.
+ */
 app.post('/books', async (req, res) => {
     const book = await Book.create(req.body);
     res.json(book);
 });
 
+/**
+ * Ruta para actualizar un libro por ID.
+ * 
+ * @route PUT /books/:id
+ * @param {Request} req - La solicitud HTTP.
+ * @param {Response} res - La respuesta HTTP.
+ */
 app.put('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -52,6 +87,13 @@ app.put('/books/:id', async (req, res) => {
     }
 });
 
+/**
+ * Ruta para eliminar un libro por ID.
+ * 
+ * @route DELETE /books/:id
+ * @param {Request} req - La solicitud HTTP.
+ * @param {Response} res - La respuesta HTTP.
+ */
 app.delete('/books/:id', async (req, res) => {
     const book = await Book.findByPk(req.params.id);
     if (book) {
@@ -62,6 +104,11 @@ app.delete('/books/:id', async (req, res) => {
     }
 });
 
+/**
+ * Inicia el servidor y escucha en el puerto especificado.
+ * 
+ * @param {number} port - El puerto en el que el servidor escucha.
+ */
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+    console.log(`Server listening on port ${port}`);
 });
